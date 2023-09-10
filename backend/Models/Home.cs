7ba89de62d46902a61ba1414b41256feee4082fe
vitor-gabriel <gabrijac57@gmail.com>
2023-09-10 17:28:02 -0400

    home endpoint

---
+++ backend/Models/Home.cs
@@ -0,0 +1,7 @@
+namespace wiki_blue_exorcist.Models;
+
+public class Home {
+    public string Title { get; set; }
+    public string Sinopse { get; set; }
+    public string ImageLocation { get; set; }
+}
